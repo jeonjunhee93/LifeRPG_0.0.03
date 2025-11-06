7ed88d62dfb33b34c692826c158c42c4a097f0d1
jeonjunhee93 <lllllljz@naver.com>
2025-11-07 02:31:44 +0900

    Add files via upload

---
+++ LifeRPG_Complete_Vercel_Project/src/main.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>Life R.P.G</h1>
+      <p>이곳에 게임 내용을 구현하세요.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
